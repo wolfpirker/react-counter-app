ca6157ecc47ffe33ef12abec36083b490f35aaab
wolfpirker <w_pirker@gmx.de>
2021-10-29 21:08:44 +0200

    Handling and binding events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,13 +4,23 @@ class Counter extends React.Component {
     state = { 
         count: 0,
         tags: ['tags1', 'tags2', 'tags3']
-     };
+    };
+
+    constructor() {
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
+    }
+
+    handleIncrement() {
+        console.log('Increment Clicked', this);        
+
+    }
 
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">increment</button>
                 <ul>
                     { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
                 </ul>
