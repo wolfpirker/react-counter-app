171635f15a10ea4c84ae3db737b2692f9ef556d7
wolfpirker <w_pirker@gmx.de>
2021-11-05 00:45:47 +0100

    having multiple components in sync, lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counters';
+import NavBar from './components/navbar';
+import React from 'react';
 
+class App extends React.Component {
+  state = {
+    counters: [
+      { id: 1, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    // cloning counters -> the objects are exactly the same as above
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+        <main className="container">
+          <Counter
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete} />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+/*
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <NavBar />
+    <main className="container">
+     <Counter />
+    </main>
+    </React.Fragment>
   );
 }
+*/
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
