3ef33a311fb140886985382ff3f2ef1ed69ff4e0
wolfpirker <w_pirker@gmx.de>
2021-11-06 03:02:16 +0100

    stateless function component and destructuring arguments to improve simplicity of code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+
+// Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary">
+          {this.props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
